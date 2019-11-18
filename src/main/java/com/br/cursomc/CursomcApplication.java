b911488af0da421aacccd1a99145f3c2ea4e5a7a
MICHELANGELO MENDES CHAVES <michelangelo.mendes.chaves@everis.com>
2019-11-18 20:34:37 -0200

    Criando entidade de produto e realciomanemento com categoria

--- src/main/java/com/br/cursomc/CursomcApplication.java
+++ src/main/java/com/br/cursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.br.cursomc.domain.Categoria;
+import com.br.cursomc.domain.Produto;
 import com.br.cursomc.repositories.CategoriaRepository;
+import com.br.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -22,9 +26,22 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
 		Categoria categoriaI = new Categoria(null, "Infomatica");
 		Categoria categoriaE = new Categoria(null, "Escritório");
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
+
+		categoriaI.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		categoriaE.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(categoriaI));
+		p2.getCategorias().addAll(Arrays.asList(categoriaI, categoriaE));
+		p3.getCategorias().addAll(Arrays.asList(categoriaI));
+
 		categoriaRepository.saveAll(Arrays.asList(categoriaI, categoriaE));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 	}
 }
--- src/main/java/com/br/cursomc/domain/Produto.java
+++ src/main/java/com/br/cursomc/domain/Produto.java
@@ -8,31 +8,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
-	private static final long serialVersionUID = 2362399190854645425L;
+public class Produto implements Serializable {
 
-	public Categoria(Integer id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
-	}
-
-	public Categoria() {
-		super();
-	}
+	private static final long serialVersionUID = -2130891224905272269L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
 	private String nome;
-    
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
-	
+	private Double preco;
+
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+	  joinColumns = @JoinColumn(name = "produto_id"),
+	  inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
+
+	public Produto() {
+		super();
+	}
+
+	public Produto(Integer id, String nome, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -47,6 +54,22 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -65,26 +88,13 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Categoria [id=" + id + ", nome=" + nome + "]";
-	}
-
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
 	}
 
 }
